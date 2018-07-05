d92ff2fd3163eaa6dd7f6923da4cc1e82227e19b
Dharma106 <20860882+Dharma106@users.noreply.github.com>
2018-07-05 18:33:55 +0530

    Update equity.py

--- equity.py
+++ equity.py
@@ -1,7 +1,9 @@
+# Stock price webscrapping
 import requests
 from bs4 import BeautifulSoup
 import locale
 locale.setlocale(locale.LC_ALL, "")
+import pandas as pd
 
 def nse_equity_output():
     page_address= "https://www.moneycontrol.com/india/stockpricequote/"
@@ -12,6 +14,9 @@ def nse_equity_output():
                  "CDSL":"finance-investments/centraldepositoryservicesltd/CDS",
                  "Indian Overseas":"banks-public-sector/indianoverseasbank/IOB",
                  "Sintex Plastics":"plastics/sintexplasticstechnology/SPT"}    
+    equity_name = []
+    price = []
+    volume =[]
     
     for i in range(len(stock_dict)):
         web_page = page_address + stock_dict.values()[i]
@@ -22,5 +27,12 @@ def nse_equity_output():
         sp_nse_price = float(sp_nse_price)
         sp_nse_volumes= sp_nse_content.find(class_="gD_12").find("strong").get_text()
         sp_nse_volumes= locale.atoi(sp_nse_volumes)
-        print({str(stock_dict.keys()[i]+' Volume'):sp_nse_volumes, 
-               str(stock_dict.keys()[i]+' Price'):sp_nse_price})
+        equity_name.append(stock_dict.keys()[i])
+        price.append(sp_nse_price)
+        volume.append(sp_nse_volumes)
+        
+    equity_data = {'Equity Name': equity_name,
+               'Price': price,
+               'Volumes': volume}
+    equity_detail_table = pd.DataFrame(data = equity_data)
+    print(equity_detail_table)        
