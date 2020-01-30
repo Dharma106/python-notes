e97cdb6ac43a98f59247fbc6a2cb6a843a5c5bc9
Dharma106 <20860882+Dharma106@users.noreply.github.com>
2020-01-30 17:18:07 +0530

    Create print_kite_shape_star.py

---
+++ print_kite_shape_star.py
@@ -0,0 +1,20 @@
+def print_star(n):
+    if n % 2 == 1:
+        if n == 3:
+            start_pos = 2
+        else :
+            start_pos = 2 + n//3        
+        # end_pos = n        
+        print_pos_array = [list(range(start_pos-i, start_pos+i+1))
+                      for i in range(1, start_pos)]      
+        print_pos_array.insert(0, [start_pos])
+        temp_array = print_pos_array.copy()
+        temp_array = temp_array[:-1]
+        temp_array.reverse()
+        print_pos_array = print_pos_array + temp_array
+        
+        for arr in print_pos_array:
+            array_len = len(arr)
+            print(" "*arr[0] + "*"*array_len)
+    else:
+        print("number provided is even try with odd number")
