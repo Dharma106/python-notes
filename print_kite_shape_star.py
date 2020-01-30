5ebc278c19fd47f7e81c47f6e3a7796dd3336826
Dharma106 <20860882+Dharma106@users.noreply.github.com>
2020-01-30 17:31:37 +0530

    Update print_kite_shape_star.py

--- print_kite_shape_star.py
+++ print_kite_shape_star.py
@@ -3,16 +3,13 @@ def print_star(n):
         if n == 3:
             start_pos = 2
         else :
-            start_pos = 2 + n//3        
-        # end_pos = n        
+            start_pos = 2 + n//3     
         print_pos_array = [list(range(start_pos-i, start_pos+i+1))
                       for i in range(1, start_pos)]      
-        print_pos_array.insert(0, [start_pos])
-        temp_array = print_pos_array.copy()
-        temp_array = temp_array[:-1]
+        print_pos_array.insert(0, [start_pos])        
+        temp_array = print_pos_array[:-1]
         temp_array.reverse()
-        print_pos_array = print_pos_array + temp_array
-        
+        print_pos_array = print_pos_array + temp_array        
         for arr in print_pos_array:
             array_len = len(arr)
             print(" "*arr[0] + "*"*array_len)
